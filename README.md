d8b63a64b3183c436079f2ac7a9338141ba53086
Parkhyunwoo <parkhj0423@naver.com>
2023-02-26 16:40:50 +0900

    [parkhj0423] Initial Commit

--- README.md
+++
@@ -1,1 +0,0 @@
-# Short-Form-Video-UI
---
+++ Short Form Video UI/Short Form Video UI/Short_Form_Video_UIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Short_Form_Video_UIApp.swift
+//  Short Form Video UI
+//
+//  Created by 박현우 on 2023/02/26.
+//
+
+import SwiftUI
+
+@main
+struct Short_Form_Video_UIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
