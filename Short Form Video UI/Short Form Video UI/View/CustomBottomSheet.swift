5288cc34bc3869c84bf8902d718714351a9dbc0e
Parkhyunwoo <parkhj0423@naver.com>
2023-03-03 23:31:04 +0900

    [parkhj0423] CustomBottomSheet 구현을 위한 파일 생성

---
+++ Short Form Video UI/Short Form Video UI/View/CustomBottomSheet.swift
@@ -0,0 +1,20 @@
+//
+//  CustomBottomSheet.swift
+//  Short Form Video UI
+//
+//  Created by 박현우 on 2023/03/03.
+//
+
+import SwiftUI
+
+struct CustomBottomSheet: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CustomBottomSheet_Previews: PreviewProvider {
+    static var previews: some View {
+        CustomBottomSheet()
+    }
+}
